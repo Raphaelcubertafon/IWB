3895a897cd82190c03e184bc5ed41ace5c1d7963
Waves59 <thibault.demars59@gmail.com>
2018-04-09 16:26:03 +0200

    make it better

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
 
+  def home
+    @pages = Page.all
+  end
+
   # GET /pages
   # GET /pages.json
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable  
-
-         validates :username, presence: true, length: {maximum: 50}
+         :recoverable, :rememberable, :trackable, :validatable
 end
